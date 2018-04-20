91a1be4cce5cec7d9ba9fc318bc1f35fadafddd0
KVISDAOWNER <krijen16@student.aau.dk>
2018-04-20 13:04:08 +0200

    Symboltables insert now checks if the parameter of functions and strategies exists in the global scope

--- src/FIRe/SymbolTable.java
+++ src/FIRe/SymbolTable.java
@@ -41,6 +41,11 @@ public class SymbolTable  {
                 else
                     returnType = "void";
 
+                for (Tuple<String, String> param: fparams) {
+                    //Here we can use peek, because we know a function declaration will only appear at the global scope
+                    if(stack.Peek().containsKey(param.x))
+                        throw new Exception("Variable name in function parameter already used in global scope");
+                }
                 stack.Peek().put(idNode.name, new SymbolData(input, returnType, fparams));
 
             }
@@ -53,6 +58,11 @@ public class SymbolTable  {
             else if(input instanceof StrategyDeclarationNode){
                 IdNode idNode = (IdNode) input.Id;
                 ArrayList<Tuple<String,String>> sparams = GetParams(input);
+                for (Tuple<String, String> param: sparams) {
+                    //Here we can use peek, because we know a strategy declaration will only appear at the global scope
+                    if(stack.Peek().containsKey(param.x))
+                        throw new Exception("Variable name in strategy parameter already used in global scope");
+                }
                 stack.Peek().put(idNode.name, new SymbolData(input,sparams));
             }
             else
