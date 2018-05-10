37dfeb4552dfff586fc30916e619aaa451954ec4
KVISDAOWNER <krijen16@student.aau.dk>
2018-05-10 11:04:14 +0200

    CodeGeneration: Only adds a condition once to the enum

--- src/FIRe/CodeGeneration/Enum.java
+++ src/FIRe/CodeGeneration/Enum.java
@@ -1,5 +1,7 @@
 package FIRe.CodeGeneration;
 
+import java.util.ArrayList;
+
 public class Enum extends Indenter{
     Enum(String name){
         this.name = name;
@@ -8,14 +10,25 @@ public class Enum extends Indenter{
     boolean isThisFirstValue = true;
     String name;
     StringBuilder sb = new StringBuilder();
+    ArrayList<String> values = new ArrayList<String>();
     public void addValue(String val){
-        if (isThisFirstValue){
-            sb.append(val);
-            isThisFirstValue = false;
+        if (!isValueDefined(val)){
+            values.add(val);
+            if (isThisFirstValue){
+                sb.append(val);
+                isThisFirstValue = false;
+            }
+            else
+                sb.append(", " + val);
         }
-        else
-            sb.append(", " + val);
     }
+
+    private boolean isValueDefined(String val) {
+        if(values.contains(val))
+            return true;
+        return false;
+    }
+
     @Override
     public String toString(){
         return "public enum " + name + "{\n" +
